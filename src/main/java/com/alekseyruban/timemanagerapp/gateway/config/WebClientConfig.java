869cec19cf8b19975c7e5d8dddbf4e032baff360
aleksey-ruban <alekseyruban555@gmail.com>
2025-12-18 23:20:15 +0300

    Using load balancer + eureka and actuator fix

--- src/main/java/com/alekseyruban/timemanagerapp/gateway/config/WebClientConfig.java
+++ src/main/java/com/alekseyruban/timemanagerapp/gateway/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.alekseyruban.timemanagerapp.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,9 +9,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
     public WebClient authWebClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("http://localhost:8081")
+                .baseUrl("http://auth-service")
                 .build();
     }
 }
